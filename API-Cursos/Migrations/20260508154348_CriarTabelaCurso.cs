43fd9bdc59ad6be2abedc7836ef5d896df4cdb3b
unknown <gabriel@DSN1191348083.edu119.local>
2026-05-08 12:44:44 -0300

    adicionei migrations

---
+++ API-Cursos/Migrations/20260508154348_CriarTabelaCurso.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Cursos.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarTabelaCurso : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "tabela_cursos",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint(20)", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nome = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false, collation: "utf8mb4_uca1400_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    periodo = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false, collation: "utf8mb4_uca1400_ai_ci")
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    is_ativo = table.Column<ulong>(type: "bit(1)", nullable: false, defaultValueSql: "b'1'")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PRIMARY", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .Annotation("Relational:Collation", "utf8mb4_uca1400_ai_ci");
+
+            migrationBuilder.CreateIndex(
+                name: "nome",
+                table: "tabela_cursos",
+                column: "nome",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tabela_cursos");
+        }
+    }
+}
